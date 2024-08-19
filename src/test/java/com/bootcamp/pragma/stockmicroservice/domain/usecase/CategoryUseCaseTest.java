441bea25f0a66d6bc91cfc936f37f24939292a13
3lPr1mo <erickenriquechaparro@gmail.com>
2024-08-19 18:16:55 -0500

    Fix: SonarLint

--- src/test/java/com/bootcamp/pragma/stockmicroservice/domain/usecase/CategoryUseCaseTest.java
+++ src/test/java/com/bootcamp/pragma/stockmicroservice/domain/usecase/CategoryUseCaseTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
