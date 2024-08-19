daee437c62732037a1efcb67d466c43552c53afb
3lPr1mo <erickenriquechaparro@gmail.com>
2024-08-19 18:14:22 -0500

    Test: List categories from domain

--- src/test/java/com/bootcamp/pragma/stockmicroservice/domain/usecase/CategoryUseCaseTest.java
+++ src/test/java/com/bootcamp/pragma/stockmicroservice/domain/usecase/CategoryUseCaseTest.java
@@ -9,8 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
@@ -39,6 +42,22 @@ class CategoryUseCaseTest {
         Category category = new Category(1L, "Comida", "Pa' comer");
         when(categoryPersistencePort.findCategoryByName(category.getName())).thenReturn(Optional.of(category));
         assertThrows(CategoryAlreadyExistError.class, () -> categoryUseCase.saveCategory(category));
+    }
+
+    @Test
+    void shouldReturnAllCategoriesWhenExist() {
+        int page = 0;
+        int size = 10;
+        boolean isAsc = true;
+        List<Category> expectedCategories = List.of(
+                new Category(1L, "Comida", "Para comer"),
+                new Category(2L, "Electrodomesticos", "Para eletrificarse")
+        );
+
+        when(categoryPersistencePort.findAllCategories(page, size, isAsc)).thenReturn(expectedCategories);
+
+        List<Category> actualCategories = categoryUseCase.findAllCategories(page, size, isAsc);
+        assertEquals(expectedCategories, actualCategories);
     }
 
 }
