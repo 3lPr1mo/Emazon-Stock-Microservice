f95055a86ab01974b6caaa989daba866c7a28d37
3lPr1mo <erickenriquechaparro@gmail.com>
2024-08-24 17:50:23 -0500

    Feat: brand infrastructure jpa

--- src/main/java/com/bootcamp/pragma/stockmicroservice/infrastructure/output/jpa/repository/BrandRepository.java
+++ src/main/java/com/bootcamp/pragma/stockmicroservice/infrastructure/output/jpa/repository/BrandRepository.java
@@ -1,4 +1,10 @@
 package com.bootcamp.pragma.stockmicroservice.infrastructure.output.jpa.repository;
 
-public interface BrandRepository {
+import com.bootcamp.pragma.stockmicroservice.infrastructure.output.jpa.entity.BrandEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BrandRepository extends JpaRepository<BrandEntity, Long> {
+    Optional<BrandEntity> findByName(String name);
 }
