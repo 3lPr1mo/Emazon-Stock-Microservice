45824a8ee4966850f6d814907571d1c0f6a919cf
3lPr1mo <erickenriquechaparro@gmail.com>
2024-08-24 13:43:01 -0500

    Fix: Cleaning responsabilities & code

--- src/main/java/com/bootcamp/pragma/stockmicroservice/infrastructure/exceptionhandler/CategoryControllerAdvisor.java
+++ src/main/java/com/bootcamp/pragma/stockmicroservice/infrastructure/exceptionhandler/CategoryControllerAdvisor.java
@@ -1,17 +1,13 @@
 package com.bootcamp.pragma.stockmicroservice.infrastructure.exceptionhandler;
 
 import com.bootcamp.pragma.stockmicroservice.domain.exception.CategoryAlreadyExistError;
-import com.bootcamp.pragma.stockmicroservice.domain.exception.CategoryFieldExceedsLimitError;
 import com.bootcamp.pragma.stockmicroservice.domain.exception.NoDataFoundException;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class CategoryControllerAdvisor {
@@ -21,17 +17,8 @@ public class CategoryControllerAdvisor {
         ExceptionResponse response = new ExceptionResponse(
           ex.getMessage(),
           HttpStatus.BAD_REQUEST.toString(),
+          HttpStatus.BAD_REQUEST.value(),
           LocalDateTime.now()
-        );
-        return ResponseEntity.badRequest().body(response);
-    }
-
-    @ExceptionHandler(CategoryFieldExceedsLimitError.class)
-    public ResponseEntity<ExceptionResponse> handleCategoryNameExceedsLimitException(CategoryFieldExceedsLimitError ex) {
-        ExceptionResponse response = new ExceptionResponse(
-                ex.getMessage(),
-                HttpStatus.BAD_REQUEST.toString(),
-                LocalDateTime.now()
         );
         return ResponseEntity.badRequest().body(response);
     }
@@ -41,29 +28,10 @@ public class CategoryControllerAdvisor {
         ExceptionResponse response = new ExceptionResponse(
                 ex.getMessage(),
                 HttpStatus.BAD_REQUEST.toString(),
+                HttpStatus.BAD_REQUEST.value(),
                 LocalDateTime.now()
         );
         return ResponseEntity.badRequest().body(response);
-    }
-
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<Map<String, String>> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
-        Map<String, String> response = new HashMap<>();
-        String error = "error";
-        if(ex.getCause() instanceof org.hibernate.exception.ConstraintViolationException){
-            String message = ex.getCause().getMessage();
-
-            if(message.contains("Duplicate entry")){
-                response.put(error, "Name already exist");
-            } else {
-                response.put(error, "Error data integrity");
-            }
-        } else {
-            response.put(error, "Error data integrity");
-        }
-
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-
     }
 
 }
