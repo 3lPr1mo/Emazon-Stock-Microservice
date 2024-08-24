a665f6b1ece6313d98b7d04fa48c9e4b89616b83
3lPr1mo <erickenriquechaparro@gmail.com>
2024-08-24 17:51:24 -0500

    Feat: Brand bean configuration

--- src/main/java/com/bootcamp/pragma/stockmicroservice/configuration/BrandBeanConfiguration.java
+++ src/main/java/com/bootcamp/pragma/stockmicroservice/configuration/BrandBeanConfiguration.java
@@ -1,4 +1,30 @@
 package com.bootcamp.pragma.stockmicroservice.configuration;
 
+import com.bootcamp.pragma.stockmicroservice.domain.api.IBrandServicePort;
+import com.bootcamp.pragma.stockmicroservice.domain.api.usecase.BrandUseCase;
+import com.bootcamp.pragma.stockmicroservice.domain.spi.IBrandPersistencePort;
+import com.bootcamp.pragma.stockmicroservice.infrastructure.output.jpa.adapter.BrandJpaAdapter;
+import com.bootcamp.pragma.stockmicroservice.infrastructure.output.jpa.mapper.BrandEntityMapper;
+import com.bootcamp.pragma.stockmicroservice.infrastructure.output.jpa.repository.BrandRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
 public class BrandBeanConfiguration {
+
+    private final BrandRepository brandRepository;
+    private final BrandEntityMapper brandEntityMapper;
+
+    @Bean
+    public IBrandPersistencePort brandPersistencePort() {
+        return new BrandJpaAdapter(brandRepository, brandEntityMapper);
+    }
+
+    @Bean
+    public IBrandServicePort brandServicePort() {
+        return new BrandUseCase(brandPersistencePort());
+    }
+
 }
