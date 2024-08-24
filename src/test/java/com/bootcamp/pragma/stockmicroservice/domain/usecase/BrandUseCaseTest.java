b06c21a583bbc58edf7d6d61f46133e25d39f19a
3lPr1mo <erickenriquechaparro@gmail.com>
2024-08-24 17:52:41 -0500

    Test: shouldSaveBrandWhenNotExist test passed

--- src/test/java/com/bootcamp/pragma/stockmicroservice/domain/usecase/BrandUseCaseTest.java
+++ src/test/java/com/bootcamp/pragma/stockmicroservice/domain/usecase/BrandUseCaseTest.java
@@ -1,13 +1,32 @@
 package com.bootcamp.pragma.stockmicroservice.domain.usecase;
 
+import com.bootcamp.pragma.stockmicroservice.domain.api.usecase.BrandUseCase;
+import com.bootcamp.pragma.stockmicroservice.domain.model.Brand;
+import com.bootcamp.pragma.stockmicroservice.domain.spi.IBrandPersistencePort;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static org.mockito.Mockito.*;
+
 class BrandUseCaseTest {
+
+    @Mock
+    private IBrandPersistencePort brandPersistencePort;
+    private BrandUseCase brandUseCase;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+        brandUseCase = new BrandUseCase(brandPersistencePort);
     }
+
+    @Test
+    void shouldSaveBrandWhenNotExist() {
+        Brand brand = new Brand(1L, "Apple", "Pcs goat");
+        brandUseCase.saveBrand(brand);
+        verify(brandPersistencePort, times(1)).save(brand);
+    }
+
 }
