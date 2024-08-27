81a77d90567000e4109c8373acdd3b87b4ba93fe
3lPr1mo <erickenriquechaparro@gmail.com>
2024-08-27 18:35:51 -0500

    Feat(test domain): add util class to test

--- src/test/java/com/bootcamp/pragma/stockmicroservice/domain/usecase/util/BrandTestUtil.java
+++ src/test/java/com/bootcamp/pragma/stockmicroservice/domain/usecase/util/BrandTestUtil.java
@@ -1,11 +1,33 @@
 package com.bootcamp.pragma.stockmicroservice.domain.usecase.util;
 
 import com.bootcamp.pragma.stockmicroservice.domain.model.Brand;
+import com.bootcamp.pragma.stockmicroservice.domain.model.ContentPage;
+
+import java.util.List;
 
 public class BrandTestUtil {
     private BrandTestUtil() {}
 
     public static Brand generateBrand() {
         return new Brand(1L, "Apple", "Pc's");
+    }
+
+    public static List<Brand> generateBrands() {
+        return List.of(
+                new Brand(1L, "Apple", "Pc's"),
+                new Brand(2L, "Microsoft", "Lo mismo de apple pero mas barato")
+        );
+    }
+
+    public static ContentPage<Brand> generateBrandPage() {
+        return new ContentPage<>(
+                1,
+                2,
+                0,
+                10,
+                true,
+                true,
+                generateBrands()
+        );
     }
 }
