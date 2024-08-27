c32c0654ad0c8e20df6eca1efd0559f4032d9d40
3lPr1mo <erickenriquechaparro@gmail.com>
2024-08-27 12:36:08 -0500

    Fix: NoDataFound in CategoryController throws 404 Not found.

--- src/main/java/com/bootcamp/pragma/stockmicroservice/infrastructure/exceptionhandler/CategoryControllerAdvisor.java
+++ src/main/java/com/bootcamp/pragma/stockmicroservice/infrastructure/exceptionhandler/CategoryControllerAdvisor.java
@@ -2,6 +2,7 @@ package com.bootcamp.pragma.stockmicroservice.infrastructure.exceptionhandler;
 
 import com.bootcamp.pragma.stockmicroservice.domain.exception.CategoryAlreadyExistError;
 import com.bootcamp.pragma.stockmicroservice.domain.exception.NoDataFoundException;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -25,6 +26,17 @@ public class CategoryControllerAdvisor {
 
     @ExceptionHandler(NoDataFoundException.class)
     public ResponseEntity<ExceptionResponse> handleNoDataFoundException(NoDataFoundException ex) {
+        ExceptionResponse response = new ExceptionResponse(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND.toString(),
+                HttpStatus.NOT_FOUND.value(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ExceptionResponse> handleConstraintViolationException(ConstraintViolationException ex) {
         ExceptionResponse response = new ExceptionResponse(
                 ex.getMessage(),
                 HttpStatus.BAD_REQUEST.toString(),
