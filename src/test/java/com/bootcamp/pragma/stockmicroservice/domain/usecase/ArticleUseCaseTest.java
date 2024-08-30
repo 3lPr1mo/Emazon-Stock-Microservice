9e87eb7106ac0d798aa1ae46b611c276e0ae04c1
3lPr1mo <erickenriquechaparro@gmail.com>
2024-08-30 15:01:57 -0500

    test(article usecase): test shouldSaveArticle passed

--- src/test/java/com/bootcamp/pragma/stockmicroservice/domain/usecase/ArticleUseCaseTest.java
+++ src/test/java/com/bootcamp/pragma/stockmicroservice/domain/usecase/ArticleUseCaseTest.java
@@ -1,4 +1,32 @@
 package com.bootcamp.pragma.stockmicroservice.domain.usecase;
 
-public class ArticleUseCaseTest {
+import com.bootcamp.pragma.stockmicroservice.domain.api.usecase.ArticleUseCase;
+import com.bootcamp.pragma.stockmicroservice.domain.model.Article;
+import com.bootcamp.pragma.stockmicroservice.domain.spi.IArticlePersistencePort;
+import com.bootcamp.pragma.stockmicroservice.domain.usecase.util.ArticleTestUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.*;
+
+class ArticleUseCaseTest {
+
+    @Mock
+    private IArticlePersistencePort articlePersistencePort;
+    private ArticleUseCase articleUseCase;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        articleUseCase = new ArticleUseCase(articlePersistencePort);
+    }
+
+    @Test
+    void shouldSaveArticle() {
+        Article article = ArticleTestUtil.generateArticle();
+        articleUseCase.saveArticle(article);
+        verify(articlePersistencePort, times(1)).save(article);
+    }
 }
--- src/test/java/com/bootcamp/pragma/stockmicroservice/domain/usecase/util/ArticleTestUtil.java
+++ src/test/java/com/bootcamp/pragma/stockmicroservice/domain/usecase/util/ArticleTestUtil.java
@@ -1,4 +1,17 @@
 package com.bootcamp.pragma.stockmicroservice.domain.usecase.util;
 
+import com.bootcamp.pragma.stockmicroservice.domain.model.Article;
+
 public class ArticleTestUtil {
+    private ArticleTestUtil() {}
+
+    public static Article generateArticle() {
+        return new Article(
+                1L,
+                1,
+                20.0,
+                CategoryTestUtil.generateCategories(),
+                BrandTestUtil.generateBrand()
+        );
+    }
 }
