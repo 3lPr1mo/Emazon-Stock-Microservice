5999c2ac2e7d361b669c7c803db984d006e60c31
3lPr1mo <erickenriquechaparro@gmail.com>
2024-08-31 19:51:13 -0500

    feat(infrastructure article): add logic to persistence & mapper.

--- src/main/java/com/bootcamp/pragma/stockmicroservice/infrastructure/output/jpa/adapter/ArticleJpaAdapter.java
+++ src/main/java/com/bootcamp/pragma/stockmicroservice/infrastructure/output/jpa/adapter/ArticleJpaAdapter.java
@@ -1,10 +1,19 @@
 package com.bootcamp.pragma.stockmicroservice.infrastructure.output.jpa.adapter;
 
 import com.bootcamp.pragma.stockmicroservice.domain.model.Article;
+import com.bootcamp.pragma.stockmicroservice.domain.model.ContentPage;
 import com.bootcamp.pragma.stockmicroservice.domain.spi.IArticlePersistencePort;
+import com.bootcamp.pragma.stockmicroservice.infrastructure.output.jpa.entity.ArticleEntity;
 import com.bootcamp.pragma.stockmicroservice.infrastructure.output.jpa.mapper.ArticleEntityMapper;
 import com.bootcamp.pragma.stockmicroservice.infrastructure.output.jpa.repository.ArticleRepository;
+import com.bootcamp.pragma.stockmicroservice.infrastructure.util.ArticleConstants;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class ArticleJpaAdapter implements IArticlePersistencePort {
@@ -15,5 +24,22 @@ public class ArticleJpaAdapter implements IArticlePersistencePort {
     @Override
     public void save(Article article) {
         articleRepository.save(articleEntityMapper.modelToArticleEntity(article));
+    }
+
+    @Override
+    public ContentPage<Article> findAllArticles(int page, int size, boolean isAsc, String sortBy) {
+        Sort sort = isAsc ? Sort.by(ArticleConstants.getValueSortMappingArticle(sortBy)).ascending() : Sort.by(ArticleConstants.getValueSortMappingArticle(sortBy)).descending();
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<ArticleEntity> articlesPage = articleRepository.findAll(pageable);
+        List<Article> articles = articleEntityMapper.entityToArticleModelList(articlesPage.getContent());
+        return new ContentPage<>(
+                articlesPage.getTotalPages(),
+                articlesPage.getTotalElements(),
+                articlesPage.getPageable().getPageNumber(),
+                articlesPage.getPageable().getPageSize(),
+                articlesPage.isFirst(),
+                articlesPage.isLast(),
+                articles
+        );
     }
 }
--- src/main/java/com/bootcamp/pragma/stockmicroservice/infrastructure/output/jpa/mapper/ArticleEntityMapper.java
+++ src/main/java/com/bootcamp/pragma/stockmicroservice/infrastructure/output/jpa/mapper/ArticleEntityMapper.java
@@ -4,7 +4,11 @@ import com.bootcamp.pragma.stockmicroservice.domain.model.Article;
 import com.bootcamp.pragma.stockmicroservice.infrastructure.output.jpa.entity.ArticleEntity;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface ArticleEntityMapper {
     ArticleEntity modelToArticleEntity(Article article);
+    Article entityToArticleModel(ArticleEntity articleEntity);
+    List<Article> entityToArticleModelList(List<ArticleEntity> articleEntityList);
 }
