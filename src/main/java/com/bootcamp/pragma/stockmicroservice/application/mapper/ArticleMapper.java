94999d0c5949db99f01e56851844cff4c8c148da
3lPr1mo <erickenriquechaparro@gmail.com>
2024-08-31 20:48:16 -0500

    fix(application mapper): add required methods to map to ContentPage<ArticleResponse>.

--- src/main/java/com/bootcamp/pragma/stockmicroservice/application/mapper/ArticleMapper.java
+++ src/main/java/com/bootcamp/pragma/stockmicroservice/application/mapper/ArticleMapper.java
@@ -3,8 +3,11 @@ package com.bootcamp.pragma.stockmicroservice.application.mapper;
 import com.bootcamp.pragma.stockmicroservice.application.dto.request.CreateArticle;
 import com.bootcamp.pragma.stockmicroservice.application.dto.response.ArticleResponse;
 import com.bootcamp.pragma.stockmicroservice.domain.model.Article;
+import com.bootcamp.pragma.stockmicroservice.domain.model.ContentPage;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ArticleMapper {
@@ -12,4 +15,6 @@ public interface ArticleMapper {
     Article requestToArticleModel(CreateArticle createArticle);
 
     ArticleResponse articleToArticleResponse(Article article);
+    List<ArticleResponse> modelToArticleResponseList(List<Article> articles);
+    ContentPage<ArticleResponse> modelToArticleResponsePage(ContentPage<Article> articleContentPage);
 }
