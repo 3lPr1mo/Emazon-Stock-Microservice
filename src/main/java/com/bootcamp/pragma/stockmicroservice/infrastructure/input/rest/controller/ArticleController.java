72447e963b8b765a20e3a81170b82b3c071c20aa
3lPr1mo <erickenriquechaparro@gmail.com>
2024-08-31 22:20:41 -0500

    feat(infrastructure): add controller & fix entity to fetch eager.

--- src/main/java/com/bootcamp/pragma/stockmicroservice/infrastructure/input/rest/controller/ArticleController.java
+++ src/main/java/com/bootcamp/pragma/stockmicroservice/infrastructure/input/rest/controller/ArticleController.java
@@ -2,14 +2,13 @@ package com.bootcamp.pragma.stockmicroservice.infrastructure.input.rest.controll
 
 import com.bootcamp.pragma.stockmicroservice.application.IArticleHandler;
 import com.bootcamp.pragma.stockmicroservice.application.dto.request.CreateArticle;
+import com.bootcamp.pragma.stockmicroservice.application.dto.response.ArticleResponse;
+import com.bootcamp.pragma.stockmicroservice.domain.model.ContentPage;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/article")
@@ -22,5 +21,15 @@ public class ArticleController {
     public ResponseEntity<Void> saveArticle(@Valid @RequestBody CreateArticle createArticle) {
         articleHandler.create(createArticle);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<ContentPage<ArticleResponse>> findAllArticles(
+            @RequestParam int page,
+            @RequestParam int size,
+            @RequestParam boolean isAsc,
+            @RequestParam String sortBy
+    ) {
+        return ResponseEntity.ok(articleHandler.findAllArticles(page, size, isAsc, sortBy));
     }
 }
