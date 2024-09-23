3590f98b7dfe5f445b9a0bd2bce078e9407db5d4
3lPr1mo <erickenriquechaparro@gmail.com>
2024-09-23 16:25:07 -0500

    feat(config): add config filter bean configuration

---
+++ src/main/java/com/bootcamp/pragma/stockmicroservice/configuration/security/ConfigFilter.java
@@ -0,0 +1,36 @@
+package com.bootcamp.pragma.stockmicroservice.configuration.security;
+
+import com.bootcamp.pragma.stockmicroservice.domain.model.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class ConfigFilter {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/category").hasAnyAuthority(Role.ADMINISTRADOR.name())
+                        .requestMatchers(HttpMethod.POST, "/brand").hasAnyAuthority(Role.ADMINISTRADOR.name())
+                        .requestMatchers(HttpMethod.POST, "/article").hasAnyAuthority(Role.ADMINISTRADOR.name())
+                        .anyRequest().permitAll()
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+}
